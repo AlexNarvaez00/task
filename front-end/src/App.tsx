050eaa8363de91ad9fd91761a062ce7737426c0c
Alexis <zaizealexis@gmail.com>
2025-06-08 11:31:10 -0600

    feat: redirect task

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,8 +1,20 @@
 import styles from "./App.module.css";
-import { Outlet } from "react-router";
+import { Outlet, useNavigate } from "react-router";
 import { join } from "./shared/css/CssUtils";
+import { useEffect } from "react";
+import { useBreadcrumbs } from "./app/components/useBreadcrumbs";
 
 function App() {
+  const navigate = useNavigate();
+  const path = useBreadcrumbs();
+
+  useEffect(() => {
+    const [base] = path;
+    if (base === undefined || base === "/") {
+      navigate("/task");
+    }
+  }, [path, navigate]);
+
   return (
     <section>
       <header className={join(styles.header)}></header>
